b48552666315c45d7d0d627f72ddcce048ad7e5c
Kasra Askarizadeh <126344025+KasrAskari@users.noreply.github.com>
2024-05-06 22:28:06 +0330

    Delete File Handling directory

--- File Handling/Delete a File.py
+++
@@ -1,8 +0,0 @@
-
-
-import os
-
-if os.path.exists("demofile4.txt"):
-    os.remove("demofile4.txt")
-else:
-    print("The file does not exist")
--- File Handling/File Open/Close Files.py
+++
@@ -1,9 +0,0 @@
-
-
-#? It is a good practice to always close the file when you are done with it.
-
-f = open("demofile.txt", "r")
-
-print(f.readline())
-
-f.close()
